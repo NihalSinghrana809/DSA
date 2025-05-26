3fb4c8b22a83d976803fa43599268ad2b9ce0eef
Nihal Singh Rana <nihalsinghrana809@gmail.com>
2025-05-26 09:03:27 +0530

    Add files via upload

---
+++ 1.Odd_Even.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+int main(){
+int n;
+cout<<"Enter the Number:"<<endl;
+cin>>n;
+if(n%2==0){
+    cout<<"Number is Even"<<endl;
+}
+else{
+    cout<<"Number is Odd"<<endl;
+}
+return 0;
+}
